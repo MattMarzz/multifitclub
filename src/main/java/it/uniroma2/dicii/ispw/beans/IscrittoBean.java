5e5366fe6e7c38aa0cbcdf81894329f3c7d5d15a
MattMarzz <im.matteomarzella@gmail.com>
2023-12-15 11:14:39 +0100

    first bozza to manage subscribers

---
+++ src/main/java/it/uniroma2/dicii/ispw/beans/IscrittoBean.java
@@ -0,0 +1,7 @@
+package it.uniroma2.dicii.ispw.beans;
+
+public class IscrittoBean { //let's see if need to be serializable
+    private String name;
+    private String surname;
+    //other attributes and getter and setter
+}
