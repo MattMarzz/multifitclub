30ed01696ba9bca0621fe340341274320b38ecae
MattMarzz <matteo01marzella01@gmail.com>
2024-01-25 20:13:35 +0100

    database dump

--- sql/dump25_01.sql
+++ sql/dump25_01.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `multifitclub` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `multifitclub`;
 -- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
 --
 -- Host: localhost    Database: multifitclub
@@ -191,4 +193,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-01-25 20:07:12
+-- Dump completed on 2024-01-25 20:13:22
